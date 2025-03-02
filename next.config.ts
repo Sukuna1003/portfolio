581935f857dce4100ab91c60160ed6203f90489d
Sukuna1003 <vishesh18092003@gmail.com>
2025-03-02 20:11:51 +0530

    fix: static export setup

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",  // This is crucial for static export
+  distDir:"out",
+  images: {
+    unoptimized: true,  // GitHub Pages doesn’t support Next.js optimized images
+  },
+  basePath: "/portfolio",  // This must match your repository name
+  assetPrefix: "/portfolio", // Same here
 };
 
 export default nextConfig;
